ae564e2597209bf42a2ffce2c1c9dfb1d5db7c38
Robert Martinez <djramg@gmail.com>
2024-11-30 05:24:35 +0100

    prueba github 2

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/CreaTuLanding1-Martinez/'
+  base: "/CreaTuLanding1-Martinez",
 })
